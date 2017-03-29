270ab939fc29ce333c18ef87a60a7908af7f5239
Shreyas S Bhat <shreyas15@hotmail.com>
2017-03-28 22:37:08 -0400

    Implemented Feature: Find declared variables that are not used in the code and report
    - feature implemented.
    - code cleanup
    - code refactored

--- src/Driver.java
+++ src/Driver.java
@@ -1,15 +1,17 @@
 import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 //import java.util.HashMap;
 //import java.util.Map;
-//import java.util.regex.Pattern;
-//import java.util.regex.Matcher;
-//import java.util.regex.Pattern;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 import java.util.List;
 
 public class Driver {
+	
+	private static final Pattern IDENTIFIER_RX = Pattern.compile("([a-zA-Z_$][a-zA-Z0-9_$]*)"); 
 	
 	public static void main(String[] args) throws IOException{
 		
@@ -24,7 +26,24 @@ public class Driver {
 		for(Token t:tokens){
 			t.registerVariables();
 		}
-		
+		updateMap(fileName);
+		for (Token t: tokens){
+			t.getReport();
+		}
+		br.close();
+	}
+	
+	static void updateMap(String fileName) throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		String eachLine = "";
+		while((eachLine = br.readLine()) != null){
+			Matcher matcher = IDENTIFIER_RX.matcher(eachLine);
+			while (matcher.find()){
+				String found = matcher.group().trim();
+				int counter = Token.varNames.containsKey(found)? Token.varNames.get(found) : 0;
+				Token.varNames.put(found, counter + 1);
+			}
+		}
 		br.close();
 	}
 }
--- src/Token.java
+++ src/Token.java
@@ -6,26 +6,38 @@ import java.util.regex.Pattern;
 public class Token {
 	
 	String line = "";
-	int lineNumber = -1;
-	int columnNumber = -1;
+	int lineNumber = 0;
+	int columnNumber = 0;
 	String objName = "";
 	
 	private static final Pattern VAR_BOUNDARY = Pattern.compile("(let|var)(.*?)\\=(.*?)\\;");
 	private static final Pattern VAR_BOUNDARY2 = Pattern.compile("(let|var)(.*?)\\;");
 	public static Map<String, Integer> varNames = new HashMap<String, Integer>(); 
 	
+	 public Map<String, Integer> getMap() {
+	       return varNames;
+	 }
+	
+	public Token (){
+		this.line = "";
+		this.objName = "";
+		this.lineNumber = 0;
+		this.columnNumber = 0;
+	}
+	
 	public Token (String line, int lineNum){
 		this.line = line;
 		this.objName = "";
 		this.lineNumber = lineNum;
-		this.columnNumber = -1;
+		this.columnNumber = 0;
 	}
 	
 	public void getReport(){
-		boolean flag = varNames.containsKey(this.objName) ? (varNames.get(this.objName) == 1) ? true : false : false;
+
+		boolean flag = varNames.containsKey(this.objName) ? (varNames.get(this.objName) == 2) ? true : false : false;
 		if (flag)
 			System.out.println(this.objName + " : " + "is an unused variable"  + " at line " + this.lineNumber);
-		else ;
+		else return;
 	}
 	
 	public void registerVariables(){
@@ -44,7 +56,6 @@ public class Token {
 			}
 			int counter = varNames.containsKey(this.objName)? varNames.get(this.objName) : 0;
 			varNames.put(this.objName, counter + 1);
-			System.out.println(this.objName);
 		}
 		else if (matcher2.matches()){
 			splitter = line.split("\\;")[0];
@@ -57,7 +68,9 @@ public class Token {
 			}
 			int counter = varNames.containsKey(this.objName)? varNames.get(this.objName) : 0;
 			varNames.put(this.objName, counter + 1);
-			System.out.println(this.objName);
 		}
 	}
+	
+	
 }
+
